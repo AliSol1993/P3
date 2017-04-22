05166e49346956b683888e75634e4af5fb350c07
AliSol1993 <ali.soltani@haw-hamburg.de>
2017-04-22 15:46:07 +0200

    version 1
    versiona.1

--- src/Nadelbaum.java
+++ src/Nadelbaum.java
@@ -4,7 +4,38 @@ public class Nadelbaum
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+
+		nadelbaum(5);
+
+	}
+
+	public static void nadelbaum(int hohe)
+	{
+		int i;
+		int b;
+
+		for(i = 0; i < hohe - 1; i++)
+		{
+
+			for(int c = 1; c < hohe - i; c++)
+			{
+				System.out.print(".");
+
+			}
+
+			for(b = 0 ; b  < hohe + i; b++)
+			{
+				System.out.print("*");
+			}
+
+			for(int d = 1; d < hohe - i; d++)
+			{
+				System.out.print(".");
+
+			}
+			
+			System.out.println();
+		}
 
 	}
 
